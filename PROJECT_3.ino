71a4f8d8466e2a2cefc9be66591ea03f58ec03f5
Saksham-Mihani <104569659+Saksham-Mihani@users.noreply.github.com>
2023-03-11 09:38:08 +0530

    Update PROJECT_3.ino

--- PROJECT_3.ino
+++ PROJECT_3.ino
@@ -1,26 +1,3 @@
-#include <RadioHead.h>
-#include <radio_config_Si4460.h>
-#include <RHCRC.h>
-#include <RHDatagram.h>
-#include <RHGenericDriver.h>
-#include <RHGenericSPI.h>
-#include <RHHardwareSPI.h>
-#include <RHMesh.h>
-#include <RHNRFSPIDriver.h>
-#include <RHReliableDatagram.h>
-#include <RHRouter.h>
-#include <RHSoftwareSPI.h>
-#include <RHSPIDriver.h>
-#include <RHTcpProtocol.h>
-#include <RH_ASK.h>
-#include <RH_NRF24.h>
-#include <RH_NRF905.h>
-#include <RH_RF22.h>
-#include <RH_RF24.h>
-#include <RH_RF69.h>
-#include <RH_RF95.h>
-#include <RH_Serial.h>
-#include <RH_TCP.h>
 #include <RH_ASK.h> // Include RadioHead Amplitude Shift Keying Library
 #include <SPI.h> // Include dependant SPI Library
 int state = 0;
