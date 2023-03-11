2a605a1234ebf753f9c276d1dc0d1c7f03f46342
Saksham-Mihani <104569659+Saksham-Mihani@users.noreply.github.com>
2023-03-11 09:38:48 +0530

    Update PROJECT_3.ino

--- PROJECT_3.ino
+++ PROJECT_3.ino
@@ -3,20 +3,20 @@
 int state = 0;
 // Create Amplitude Shift Keying Object
 RH_ASK driver;
-//int msensor = A1;
-//int msvalue = 0;
-//int led = 13;
+int msensor = A1;
+int msvalue = 0;
+int led = 13;
 void setup() {
 Serial.begin(9600);
-//pinMode(msensor,INPUT);
-//pinMode(led,OUTPUT);
+pinMode(msensor,INPUT);
+pinMode(led,OUTPUT);
 driver.init();
 Serial.begin(9600);
 }
 
 void loop() {
-//msvalue = analogRead(msensor);
-//Serial.println(msvalue);
+msvalue = analogRead(msensor);
+Serial.println(msvalue);
  uint16_t data = 100;
  driver.send((uint8_t *)&data, sizeof(data));
  driver.waitPacketSent();
